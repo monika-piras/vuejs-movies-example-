1dbb0884f3bee76a8440a8dc022529eb427d26fe
Monica Piras <monica.piras.info@gmail.com>
2024-03-20 19:33:28 +0100

    add demo

---
+++ dist/precache-manifest.074e32c0adb636fef285b2202d6b5e59.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b836c85880223d474930",
+    "url": "/vuejs-movies-example/css/app.81bda5ba.css"
+  },
+  {
+    "revision": "b8f09b59c5a322d73c03",
+    "url": "/vuejs-movies-example/css/chunk-vendors.2c842817.css"
+  },
+  {
+    "revision": "ca63eb2b36b749543d28617a470f8220",
+    "url": "/vuejs-movies-example/films.json"
+  },
+  {
+    "revision": "93bc68aae34571a0ac504a0de4cdf738",
+    "url": "/vuejs-movies-example/img/cannes.jpg"
+  },
+  {
+    "revision": "7ef67a9637aac1f5d83c5adb923cbea2",
+    "url": "/vuejs-movies-example/img/extended-family.jpg"
+  },
+  {
+    "revision": "aae764b0be7fbf06887fc53b4f0b0ca2",
+    "url": "/vuejs-movies-example/img/fubar.jpg"
+  },
+  {
+    "revision": "7024c8793c3200e24f239ef4903489ca",
+    "url": "/vuejs-movies-example/img/good-omens.jpg"
+  },
+  {
+    "revision": "f2264ec2dc2a9f95bcc1f16923241137",
+    "url": "/vuejs-movies-example/img/house-of-the-dragon1.jpg"
+  },
+  {
+    "revision": "86418e42913666e4a954f219d05bd3c5",
+    "url": "/vuejs-movies-example/img/house-of-the-dragon2.jpg"
+  },
+  {
+    "revision": "6e3895006b183956fe4d102207b29587",
+    "url": "/vuejs-movies-example/img/lord-of-the-rings1.jpg"
+  },
+  {
+    "revision": "1ecbdf0837a3f88217988b6052027d39",
+    "url": "/vuejs-movies-example/img/monarch.jpg"
+  },
+  {
+    "revision": "6d6b001e15db769a888c09de64ae9910",
+    "url": "/vuejs-movies-example/img/new-girl.jpg"
+  },
+  {
+    "revision": "1a6ef3a1e0739dbbc687ab78aafe86b0",
+    "url": "/vuejs-movies-example/img/obliterated.jpg"
+  },
+  {
+    "revision": "2dd470e6afc472676e185ff42c1e0990",
+    "url": "/vuejs-movies-example/img/outlander.jpg"
+  },
+  {
+    "revision": "dd1c41f3c01f199759db6550c7899f2b",
+    "url": "/vuejs-movies-example/img/reacher1.jpg"
+  },
+  {
+    "revision": "8e6108e05f71d42adfc2d80f6a574ed1",
+    "url": "/vuejs-movies-example/img/reacher2.jpg"
+  },
+  {
+    "revision": "565e9782f04af986da42ef5041253469",
+    "url": "/vuejs-movies-example/img/space-force.jpg"
+  },
+  {
+    "revision": "8539064f28ce86abe2fb2e4caef2f552",
+    "url": "/vuejs-movies-example/img/the-lord-of-the-rings2.jpg"
+  },
+  {
+    "revision": "83fbe231631ec42ddf3628ab15f6cc0e",
+    "url": "/vuejs-movies-example/img/the-lovers.jpg"
+  },
+  {
+    "revision": "ff425545d3afd2b36a3998ca51a19e52",
+    "url": "/vuejs-movies-example/img/the-witcher1.jpg"
+  },
+  {
+    "revision": "468f0bc79001f745e16cd55df681b79c",
+    "url": "/vuejs-movies-example/img/the-witcher2.jpg"
+  },
+  {
+    "revision": "b276a42804de2c608f2988b1f61890b1",
+    "url": "/vuejs-movies-example/index.html"
+  },
+  {
+    "revision": "b836c85880223d474930",
+    "url": "/vuejs-movies-example/js/app.b8283c01.js"
+  },
+  {
+    "revision": "b8f09b59c5a322d73c03",
+    "url": "/vuejs-movies-example/js/chunk-vendors.819541bf.js"
+  },
+  {
+    "revision": "0bd0a508d3ddaaaf5140b7cc79fa0e28",
+    "url": "/vuejs-movies-example/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuejs-movies-example/robots.txt"
+  }
+]);
